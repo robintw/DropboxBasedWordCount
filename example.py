5ff9077e01460e363efe0fa98f9abe85cb3acf45
Robin Wilson <robin@rtwilson.com>
2015-12-13 11:54:52 +0000

    PEP8, plus gitignore

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
 from download import DropboxDownloader
 
 d = DropboxDownloader('/Users/robin/ThesisFilesDropboxLog')
-d.download_history_for_files("/Users/robin/Dropbox/_PhD/_FinalThesis", "*.tex", "/Users/robin/Dropbox/")
+d.download_history_for_files("/Users/robin/Dropbox/_PhD/_FinalThesis",
+                             "*.tex",
+                             "/Users/robin/Dropbox/")
