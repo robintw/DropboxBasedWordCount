5896ce493a7f89fdc9740cf34042e96fe87fbca5
Robin Wilson <robin@rtwilson.com>
2015-12-13 20:29:00 +0000

    Added more docs

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 from download import DropboxDownloader
 
+# Initialise the object and give it the folder to store its downloads in
 d = DropboxDownloader('/Users/robin/ThesisFilesDropboxLog')
-d.download_history_for_files("/Users/robin/Dropbox/_PhD/_FinalThesis",
-                             "*.tex",
-                             "/Users/robin/Dropbox/")
+
+# Download all available previous versions
+d.download_history_for_files("/Users/robin/Dropbox/_PhD/_FinalThesis",  # Folder containing files to download
+                             "*.tex",  # 'glob' string specifying files to download
+                             "/Users/robin/Dropbox/")  # Path to your Dropbox folder
